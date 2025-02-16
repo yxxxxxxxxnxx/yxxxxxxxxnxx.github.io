aca44eba3d64f78170e7efe2164a0bc1e3d643a0
yxxxxxxxxnxx <yxxxxxxxxnxx@users.noreply.github.com>
2025-02-16 22:06:47 +0000

    deploy: 32522aed2a32b157032d92ac709c0b31532d248b

--- assets/js/random-video.js
+++ assets/js/random-video.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
   const videos = [
-    '/assets/videos/seoul-1.mp4',
-    '/assets/videos/seoul-2.mp4',
-    '/assets/videos/seoul-3.mp4'
+    '/assets/videos/Seoul-1.mp4',
+    '/assets/videos/Seoul-2.mp4',
+    '/assets/videos/Seoul-3.mp4'
   ];
 
   const randomIndex = Math.floor(Math.random() * videos.length);
